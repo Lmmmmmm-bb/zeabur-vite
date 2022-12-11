3e3158fbcff7cfc274417e4209d8477c27cb0759
_lmmmmmm <lmmmmmm12138@gmail.com>
2022-12-11 23:28:49 +0800

    style: base

--- src/app/index.vue
+++ src/app/index.vue
@@ -1,3 +1,6 @@
 <template>
-  <div>Hello</div>
+  <div flex h-full items-center justify-center flex-col>
+    <h3 text-3xl>Hello World</h3>
+    <p text-coolGray>Deploy by Zeabur.</p>
+  </div>
 </template>
